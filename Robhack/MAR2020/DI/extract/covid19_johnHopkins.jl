9947fbd5cb0439a4a95b2d8c959d5fd43cf34d3c
Ali R. Vahdati <arv.kavir@gmail.com>
2020-03-23 14:04:01 +0100

    Download COVID19 from John Hopkins

---
+++ Robhack/MAR2020/DI/extract/covid19_johnHopkins.jl
@@ -0,0 +1,13 @@
+include("download.jl")
+
+function covid19_johnHopkins(source="https://covid2019-api.herokuapp.com/v2/current"; outformat="json")
+  string_data = download_api(source);
+  jsonout = JSON.parse(string_data)
+  if outformat == "df"
+    return json2df(jsonout);
+  elseif outformat == "json"
+    return jsonout
+  end
+end
+
+covid19 = covid19_johnHopkins(outformat="df");
--- Robhack/MAR2020/DI/extract/download.jl
+++ Robhack/MAR2020/DI/extract/download.jl
@@ -2,6 +2,8 @@
 
 using RemoteFiles
 using HTTP
+using JSON
+using DataFrames
 
 "Download a file from a URL"
 function download_file(datafile::String, datadir::String="./"; overwrite=false)
@@ -17,8 +19,14 @@ function download_file(datafile::String, datadir::String="./"; overwrite=false)
 end
 
 "Download a file from an API"
-function download_api(source::String, datadir::String="./")
+function download_api(source::String)
   r = HTTP.request("GET", source; verbose=3);
-  r.status == 200 && (println("Connected successfully."))
-  
+  println("Status: $(r.status)")
+  return String(r.body)
+end
+
+function json2df(input::Dict)
+  cols = reduce(∩, keys.(v["data"]))
+  df = DataFrame((Symbol(c)=>getindex.(v["data"], c) for c ∈ cols)...)
+  return df
 end
