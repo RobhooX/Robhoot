17f2201d7501a3eb84311cd0046b0a888dfcf42c
Carlos Melian <carlos.melian@eawag.ch>
2020-03-25 12:51:10 +0100

    BM update

--- Robhack/MAR2020/DI/extract/download.py
+++ Robhack/MAR2020/DI/extract/download.py
@@ -74,35 +74,131 @@ urls = ['https://www.gisaid.org/epiflu-applications/next-hcov-19-app/',
         'https://sedac.ciesin.columbia.edu/data/set/gpw-v4-population-density-rev11'
        ]
 
-
+import os
+import requests
+from mimetypes import guess_extension
 def download_file(url,path = './data'):
-   """download_file function:
+    """
+    download_file function:
 
-			This function downloads the resource from a server and places it in your local folder.
+        This function downloads the resource from a server and places it in your local folder.
 
-		Inputs:
-			>> url: URL of the desired resource to download.
-         >> path (optional): path where the resource will be stored. 
-         By default it is stored in the folder ./data/
+    Inputs:
+    
+        >> url: URL of the desired resource to download.
+        >> path (optional): path where the resource will be stored. 
+                       By default it is stored in the folder ./data/
 
-		Returns:
-			<< status: status of the request. 200 is OK.
+    Returns:
+    
+        << status: status of the request. 200 is OK.
    
-
-   """
+    """
     sread = requests.get(url,allow_redirects=True)
     status = sread.status_code
     if sread:
         print('Success! Downloading...(%s)' % url)
         header = sread.headers
-        content_type = header.get('content-type')
-        content_type = content_type.replace('application/','')
-        fname = '_'.join(url.split('/')[2:]).replace('.','_')
-        pathFname = path+'/'+fname+'.'+content_type
-        open(pathFname,'wb+').write(sread.content)
+        ext = guess_extension(sread.headers['content-type'].partition(';')[0].strip())
+        #HERE WE NEED TO CHECK IF IT IS AN HTML/HTM TO CHECK THE LINKS INSIDE AND SEE IF IT IS A RESOURCE OR NOT
+        if ext in ['html', 'htm']:
+            # get links as urls2
+            for urls in urls2:
+                download_file(url)
+        else:        
+            fname = '_'.join(url.split('/')[2:]).replace('.','_')+ext
+            pathFname = path + '/' + fname
+            # Check if directory at path exists and create it if it does not exist
+            if not os.path.exists(path):
+                os.makedirs(path)
+            # TO DO (NOT OVERWRITE DATA WITH POTENTIALLY DIFFERENT DATA FROM THE SAME URL THAT COULD BE UPDATED)
+            # Here we would need a pipeline to update/save the data so that we do not overwrite data already downloaded
+            #elif os.path.isfile(pathFname):
+                #fname = '_'.join(url.split('/')[2:]).replace('.','_')+'0'+ext
+                #pathFname = path + '/' + fname
+            print('Saving in '+pathFname)
+            open(pathFname,'wb+').write(sread.content)
     else:
-        print('An error has occurred!')
-    return sread.status_code
+        print('An error has occurred with status code %i' % status)
+    return status
 
 for url in urls:
-    download_file(url)
+    download_file(url)   
+
+ 
+
+ ###### CODE FOR ENVIRONMENTAL DATA, DIRECTORY
+
+from bs4 import BeautifulSoup,SoupStrainer
+url='https://envidatrepo.wsl.ch/uploads/chelsa/'
+
+#### Return [False,[]] if it is not html
+#### Return [True,[list of links]] if it is html
+
+def getlinks(sread):
+    """
+    getlinks function:
+
+        This function gets the links that can be found in the url to which the request sred is a response.
+
+    Inputs:
+
+        >> sread: Response of a request
+
+    Outputs:
+
+        << links: List of links inside that url
+
+    """
+    links=[]
+    for link in BeautifulSoup(sread.content, "html.parser", parse_only=SoupStrainer('a', href=True)):
+        links.append(link['href'])
+#      for link in soup.findAll('a', attrs = {'href': re.compile("^http://")}):
+#        links.append(link.get('href'))
+    return links
+
+def check_html(url):
+    """
+    check_html function:
+
+        This function checks if the url points to an html document and asks for the links inside if it is true.
+
+    Inputs:
+
+        >> url: url that we want to check
+
+    Outputs:
+
+        << l: list with 2 entries. The first is a Boolean variable 
+                l[0] = False, not html and l[1] = []
+                l[1] = True, html and l[1] = list of links inside that url
+
+    """
+    sread = requests.get(url,allow_redirects=True)
+    ext = guess_extension(sread.headers['content-type'].partition(';')[0].strip())
+    if ext not in ['.html', '.htm']:
+        return([False,[]])
+    else:
+        return([True,getlinks(sread)])
+
+
+
+max_depth = 5 ###Maximum depth we will look from the parent directory
+dout = [[] for i in range(max_depth+1)]
+dout[0] = [url]
+to_download = []
+for i in range(1,max_depth+1,1):
+   for link in dout[i-1]:
+      d = check_html(link)
+      if d[0] == False:
+         to_download.append(link)###Append to the list of downloads
+      else:
+         for j in range(len(d[1])):
+            d[1][j]=link+d[1][j]
+            dout[i] += d[1]###Add all the subdirectories to check in next depth level
+
+####Now we have a list of links to download         
+for link in to_download:
+    download_file(link)
+
+
