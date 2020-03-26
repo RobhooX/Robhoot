1efbdb9b68b69bc68eb8fae54e482f4cda1b2175
Carlos Melian <carlos.melian@eawag.ch>
2020-03-26 11:53:41 +0100

    Update costfunction.jl

--- Robhack/MAR2020/BM/costfunction.jl
+++ Robhack/MAR2020/BM/costfunction.jl
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-function costfunction(;C, max_travel_rate, infection_period = 29,
-    reinfection_probability = 0.05, detection_time = 14, death_rate = 0.02,
-    Is=[zeros(Int, C-1)..., 1], seed = 19,
-  )
-  params = create_params(C=8, max_travel_rate=0.01)
-=======
 include("RobSIR.jl")
->>>>>>> abb9ac465b7c513aab427d1dfc858dac8db3b788
 
 function cost(C, max_travel_rate)
   params = create_params(C=C, max_travel_rate=max_travel_rate)
