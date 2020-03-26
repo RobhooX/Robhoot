55d9a825005d57721190c8ef8299a1edd21fe9b9
Carlos Melian <cmelian@salmunia.ifisc.lan>
2020-03-26 11:51:11 +0100

    test zygote

--- Robhack/MAR2020/BM/costfunction.jl
+++ Robhack/MAR2020/BM/costfunction.jl
@@ -1,4 +1,4 @@
-function costfunction(;C, max_travel_rate, infection_period = 30,
+function costfunction(;C, max_travel_rate, infection_period = 29,
     reinfection_probability = 0.05, detection_time = 14, death_rate = 0.02,
     Is=[zeros(Int, C-1)..., 1], seed = 19,
   )
