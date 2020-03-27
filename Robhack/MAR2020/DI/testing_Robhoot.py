b27cdda304dec396e90a30597a869247dc0c9840
Juan <juanfernandez1984@gmail.com>
2020-03-27 15:01:38 +0100

    changed directories of data reading

--- Robhack/MAR2020/DI/testing_Robhoot.py
+++ Robhack/MAR2020/DI/testing_Robhoot.py
@@ -23,32 +23,19 @@ for url in resources:
     
 # # Data for RobSIR 1.0
 #
-# ## CAUTION: WE NEED TO IMPLEMENT THE FOLLOWING CELLS THAT READ DATA AND PUT IT IN THE FORMAT FOR ROBSIR WITH [PANDAS](https://pandas.pydata.org/) TO BE MORE EFFICIENT
-
 # ### Country ISO codes
-#
-# I will do everything with the codes that describe the country with three letters (Alpha-3 codes).
 #
 # Source: [World population review](https://worldpopulationreview.com/countries/country-codes/) They say that the codes come from the [International Organization for Standardization](https://www.iso.org/iso-3166-country-codes.html) but this organization charges for a csv of their codes.
 
 
-dfISO = pd.read_csv('./data/country_ISO_codes.csv')
-# print(dfISO.head())
-
-# country_code ={}
-# for line in fin:
-#     line = line.split(',')
-#     if line[0].split('"')[1] != 'name':
-#         country_number = int(line[3].split('"')[1])
-#         ccode = line[2].split('"')[1]
-#         country_code[country_number] = ccode
+dfISO = pd.read_csv('./data/raw_data/country_ISO_codes.csv')
 
 
 # ### Population estimates
 #
 # Source: [UN World Population Prospects 2019](https://population.un.org/wpp/Download/Standard/Population/). I did already choose the columns and rows that I wanted, but, again, this should be done via [pandas](https://pandas.pydata.org/).
 
-dfpop = pd.read_excel('./data/population_un_org_wpp_Download_Files_1_Indicators%20(Standard)_EXCEL_FILES_1_Population_WPP2019_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES_xlsx.xlsx',sheet_name='ESTIMATES',skiprows=16,usecols=["Region, subregion, country or area *",'Country code','2020'])
+dfpop = pd.read_excel('./data/raw_data/population_un_org_wpp_Download_Files_1_Indicators%20(Standard)_EXCEL_FILES_1_Population_WPP2019_POP_F01_1_TOTAL_POPULATION_BOTH_SEXES_xlsx.xlsx',sheet_name='ESTIMATES',skiprows=16,usecols=["Region, subregion, country or area *",'Country code','2020'])
 #dfpop = dfpop[["Region, subregion, country or area *",'Country code','2020']].copy()
 samp = list(dfpop['2020'])
 samp = list(map(lambda x:x*1000,samp))
@@ -62,36 +49,13 @@ for index,row in dfISO.iterrows():
     if k != False:
         codes3[k] = c1
 dfpop['Codes3'] = codes3
-# print(dfpop.head(50))
-
-
-#fin = open('./data/population_estimates_UN_2020.csv')
-# pop = {}
-# for line in fin:
-#     line = line.split(',')
-#     if line[0] != '"Region':
-#         if len(line) < 5:
-#             country_number = int(line[1])
-#             if country_number in country_code.keys():
-#                 aux = line[2].split('"')
-#                 if len(aux) == 1:
-#                     cpop = int(aux[0])*1000
-#                 else:
-#                     cpop = int(aux[1])*1000
-#                 if len(line) == 4:
-#                     aux = line[3].split('"')[0]
-#                     temp = '{:<03}'
-#                     res = temp.format(aux)
-#                     cpop += int(res            )
-#                 pop[country_code[country_number]] = cpop
 
 # ### Mobility network
 #
 # Source: [European comission Global Transnational Mobility Dataset](https://ec.europa.eu/knowledge4policy/node/35849_es). It has mobility between countries (directed) in number of trips for a year between 2011 and 2016. Just keeping the 2016 one. It is not completely symmetric, so we can think if we want to symmetrize it. Here I am storing it as a networkx directed graph. For storing I would use an edgelist although robsir is expecting a square matrix.
 
-dftrips = pd.read_csv("./data/KCMD_DDH_data_KCMD-EUI GMP_ Estimated trips.csv")
+dftrips = pd.read_csv("./data/raw_data/KCMD_DDH_data_KCMD-EUI GMP_ Estimated trips.csv")
 dftrips2 = dftrips[dftrips['time']==2016][dftrips['value']!=0]
-# print(dftrips2.head(20),len(dftrips)/6,len(dftrips2))
 names = list(np.unique(list(np.unique(dftrips2['reporting country']))+list(np.unique(dftrips2['secondary country']))))
 N = len(names)
 matr = []
@@ -144,7 +108,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime
 
-fin = open('./data/time_series_covid19_confirmed_global.csv')
+fin = open('./data/raw_data/time_series_covid19_confirmed_global.csv')
 confirmed_ts = {}
 for line in fin:
     line = line.split(',')
@@ -172,7 +136,7 @@ for country in confirmed_ts.keys():
 # In[59]:
 
 
-fin = open('./data/time_series_covid19_deaths_global.csv')
+fin = open('./data/raw_data/time_series_covid19_deaths_global.csv')
 deaths_ts = {}
 for line in fin:
     line = line.split(',')
@@ -197,10 +161,9 @@ for country in deaths_ts.keys():
     plt.plot(t, deaths_ts[country])
 
 
-# In[62]:
 
 
-fin = open('./data/time_series_covid19_recovered_global.csv')
+fin = open('./data/raw_data/time_series_covid19_recovered_global.csv')
 recovered_ts = {}
 for line in fin:
     line = line.split(',')
@@ -225,6 +188,3 @@ for line in fin:
 for country in recovered_ts.keys():
     plt.yscale('log')
     plt.plot(t, recovered_ts[country])
-
-
-# In[ ]:
