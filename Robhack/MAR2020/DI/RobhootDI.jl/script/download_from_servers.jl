951e947aabbc651225fe5a6cb2cff8f84fe0db48
Carlos Melian <carlos.melian@eawag.ch>
2020-04-02 10:23:46 +0200

    automate data discovery

--- Robhack/MAR2020/DI/RobhootDI.jl/script/download_from_servers.jl
+++ Robhack/MAR2020/DI/RobhootDI.jl/script/download_from_servers.jl
@@ -1,8 +1,12 @@
+###
+### https://github.com/JuliaWeb/JuliaWebAPI.jl
+### https://github.com/beoutbreakprepared/nCoV2019/tree/master/latest_data
 
 data_sources = Dict(
-  :covid19_johnHopkins => ["https://covid2019-api.herokuapp.com/v2/current", "json", "api"],
-  :astros => ["http://api.open-notify.org/astros.json", "json", "api"],
-  :ncov => ["http://data.nextstrain.org/ncov.json", "json", "api"]
+  :covid19Github => ["https://github.com/beoutbreakprepared/nCoV2019/blob/master/latest_data/latestdata.csv", "scv", "api"]
+  #:covid19_johnHopkins => ["https://covid2019-api.herokuapp.com/v2/current", "json", "api"],
+  #:astros => ["http://api.open-notify.org/astros.json", "json", "api"],
+  #:ncov => ["http://data.nextstrain.org/ncov.json", "json", "api"]
 )
 
 RobhootDI.download_sources(data_sources, force=false)
@@ -10,12 +14,17 @@ RobhootDI.download_sources(data_sources, force=false)
 dataframes = []
 data = readdir(datadir())
 for (index, ff) in enumerate(data)
-  if endswith(ff, ".json")
-    jsonout = JSON3.read(read(datadir(ff), String))
-    for k in keys(jsonout)
-      if typeof(jsonout[k]) <: JSON3.Array
-        df = RobhootDI.json2df(jsonout, k)
+  #if endswith(ff, ".json")
+  if endswith(ff, ".csv")
+    csvout = csv.read(read(datadir(ff), String))
+    #jsonout = JSON3.read(read(datadir(ff), String))
+    for k in keys(csvout)#(jsonout)
+       if typeof(csvout[k]) <: csv.Array
+        df = RobhootDI.csv2df(csvout, k)
         push!(dataframes, df)
+      #if typeof(jsonout[k]) <: JSON3.Array
+       # df = RobhootDI.json2df(jsonout, k)
+        #push!(dataframes, df)
       end
     end
   end
