662e4efe41a6ee8bd6d3ce1642a8bcb4a1f3561f
Ali R. Vahdati <arv.kavir@gmail.com>
2020-04-05 14:09:42 +0200

    bug fix

--- Robhack/MAR2020/BM/RobSIRs.jl/scripts/run.jl
+++ Robhack/MAR2020/BM/RobSIRs.jl/scripts/run.jl
@@ -11,6 +11,7 @@ datadir = "../../DI/data/transformed_data"
 parameters = RobSIRs.load_params(
   bs=0.0:0.0001:1.0,  # min max of uniform distribution
   ss=0.01:0.0001:1.0,
+  as=0.01:0.0001:1.0,
   dss=0.0:0.0001:1.0,
   dis=0.001:0.0001:1.0,
   drs=0.0:0.0001:1.0,
