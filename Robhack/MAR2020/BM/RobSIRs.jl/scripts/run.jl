ba395f74a7b68247268a6a45b6e0fb109589d699
Ali R. Vahdati <arv.kavir@gmail.com>
2020-04-06 14:29:44 +0200

    minor

--- Robhack/MAR2020/BM/RobSIRs.jl/scripts/run.jl
+++ Robhack/MAR2020/BM/RobSIRs.jl/scripts/run.jl
@@ -9,9 +9,9 @@ parameters = RobSIRs.load_params(
   bs=0.0:0.0001:1.0,  # min max of uniform distribution
   ss=0.01:0.0001:1.0,
   as=0.01:0.0001:1.0,
-  dss=0.0:0.0001:1.0,
+  dss=0.0:0.0,
   dis=0.001:0.0001:1.0,
-  drs=0.0:0.0001:1.0,
+  drs=0.0:0.0,
   datadir=datadir);
 
 model = create_model(parameters=parameters);
